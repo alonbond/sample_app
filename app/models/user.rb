cdd8e0715b330dc7355e6bae2c58c2c262551fdf
Alon Weissfeld <alonbond@gmail.com>
2011-11-22 23:15:17 +0200

    Added password and encrypted_password attributes to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,29 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation 
   
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
-  validates :name,  :presence   => true
+  validates :name,  :presence   => true,
                     :length     => { :maximum => 50 }             
-  validates :email, :presence   => true
-                    :format     => { :with => email_regex } 
+  validates :email, :presence   => true,
+                    :format     => { :with => email_regex }, 
                     :uniqueness => { :case_sensitive => false}
+  validates :password, :presence => true,
+                       :confirmation => true,
+                       :length => { :within => 6..40}   
 end
+
 
 # == Schema Information
 #
 # Table name: users
-# 
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
 #
 
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -7,14 +7,16 @@ class UserTest < ActiveSupport::TestCase
   end
 end
 
+
 # == Schema Information
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
 #
 
