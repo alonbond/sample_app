6cb8d862b8f2fba6866f7f9b793eab2564b50849
Alon Weissfeld <alonbond@gmail.com>
2011-11-24 15:44:51 +0200

    Added a Sessions controller and signin view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 SampleApp::Application.routes.draw do
-  
-  resources :users
 
+  resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
+  
   root :to => "pages#home"
 
   
@@ -9,6 +10,8 @@ SampleApp::Application.routes.draw do
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
   match '/signup', :to => 'users#new'
+  match '/signin', :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
   
   
    
