fcdefdf1572cbfdfbbccc54ca0c48d3b94635737
Alon Weissfeld <alonbond@gmail.com>
2011-11-26 14:53:56 +0200

    Protecting edit and update before filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -35,5 +36,13 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
-
+  
+  private
+    def authenticate
+      deny_access unless signed_in?
+    end
+    
+    def deny_access
+      redirect_to signin_path, :notice => "Please sign in to access this page." 
+    end
 end
