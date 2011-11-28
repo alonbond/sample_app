91346c9f29c53aaa370b41ce226356da3685826f
Alon Weissfeld <alonbond@gmail.com>
2011-11-28 16:47:00 +0200

    Added a Micropost model with User associations

---
+++ test/unit/micropost_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class MicropostTest < ActiveSupport::TestCase
+  # Replace this with your real tests.
+  test "the truth" do
+    assert true
+  end
+end
+
+# == Schema Information
+#
+# Table name: microposts
+#
+#  id         :integer         not null, primary key
+#  content    :string(255)
+#  user_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
