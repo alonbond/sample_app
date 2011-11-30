2445e8f0c122c0f4be584c7e5fd8e166f05dd126
Alon Weissfeld <alonbond@gmail.com>
2011-12-01 00:16:11 +0200

    Initial interface for following/followers

---
+++ test/unit/relationship_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class RelationshipTest < ActiveSupport::TestCase
+  # Replace this with your real tests.
+  test "the truth" do
+    assert true
+  end
+end
+
+# == Schema Information
+#
+# Table name: relationships
+#
+#  id          :integer         not null, primary key
+#  follower_id :integer
+#  followed_id :integer
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
